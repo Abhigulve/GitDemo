873b0f2ad9b5e44c414d554da206d655090788c4
Abhi <abhigulve06@gmail.com>
2016-09-05 19:23:46 +0530

    This code will calcute that perticular number can be represent in A^P

---
+++ JavaBasic/src/Power.java
@@ -0,0 +1,18 @@
+public class Power {
+	public static int power(int a) {
+		
+		if(a<1){
+			return 1;
+		}else{
+		for(int i=2;i<a && i<Integer.MAX_VALUE/i;i++){
+				
+		}
+		}
+		
+	return 0;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
